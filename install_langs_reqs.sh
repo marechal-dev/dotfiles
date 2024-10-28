2bdbcd91566d1e12dbef66c79883303b44e90599
marechal-dev <pietro.developer@gmail.com>
2024-10-27 23:28:29 -0300

    feat: add more scripts to automate installs

---
+++ install_langs_reqs.sh
@@ -0,0 +1,8 @@
+# Ruby
+yay -S libyaml
+
+# PHP
+yay -S autoconf bison gettext gd libcurl-openssl-1.0 libedit icu libjpeg-turbo libmysqlclient libogg oniguruma libpng postgresql-libs readline sqlite openssl libxml2 libzip re2c zlib freetype2 automake krb5 libiconv
+
+# Erlang
+yay -S ncurses glu mesa wxwidgets-gtk3 libpng libssh unixodbc libxslt fop
